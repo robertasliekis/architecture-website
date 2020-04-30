9fe0e190a2a4f8d5c0b915665c18489dde616e0d
robertasliekis <60491171+robertasliekis@users.noreply.github.com>
2020-04-30 13:58:18 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -43,19 +43,30 @@ $(window).scroll(function () {
   } else {
     mainNav.removeClass("burger-menu-inView");
   }
+  console.log($(window).scrollTop() + " tikra");
 });
 //Sticky nav bar end
 
 //Burger menu control start
 const burger = document.querySelector(".burger-menu");
 var timesClicked = 0;
+var scrollTop1 = 0;
+var scrollTop2 = 0;
 $(".burger-menu").click(function () {
+  var scrollTop1 = $(window).scrollTop();
+
   if (timesClicked % 2 != 0) {
     burger.classList.toggle("burger-menu-animation");
     burger.classList.toggle("burger-active");
     $(".menu-item").addClass("animation-reverse");
     $(".overlay-menu-wrapper").addClass("overlay-menu-invisible");
     $(".overlay-menu-wrapper").removeClass("overlay-menu-visible");
+    if (window.innerWidth <= 1024) {
+      $(".website-wrapper").removeClass("scroll-disabled");
+      document.getElementById("background-section").style.marginTop = "0px";
+      console.log(scrollTop2 + " gera2");
+      window.scrollTo(0, scrollTop2);
+    }
   } else {
     $(".overlay-menu-wrapper").removeClass("overlay-menu-invisible");
     $(".overlay-menu").addClass("keyframes-forward");
@@ -65,13 +76,20 @@ $(".burger-menu").click(function () {
     burger.classList.toggle("burger-menu-animation");
     $(".menu-item").addClass("menu-item-display");
     $(".overlay-menu-wrapper").addClass("overlay-menu-visible");
+    if (window.innerWidth <= 1024) {
+      document.getElementById("background-section").style.marginTop =
+        -scrollTop1 + "px";
+      $(".website-wrapper").addClass("scroll-disabled ");
+    }
   }
   timesClicked++;
   if (timesClicked > 1) {
     timesClicked = 0;
   }
+  scrollTop2 = scrollTop1;
 });
 //Burger menu control end
+console;
 
 //Scroll to top button start
 const btnScrollToTop = document.querySelector("#btnScrollToTop");
